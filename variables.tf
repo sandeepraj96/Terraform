eec02c839973f7be6caa48b85b6165e48499af9b
Sandy <95302937+sandeepraj96@users.noreply.github.com>
2025-01-02 13:43:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
 variable "aws_region" {
   type    = string
   default = "ap-south-1"
